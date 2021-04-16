34fbb7a3aee51c74e25a6d2efb24b881e1d4b559
Sergey Sayamov <dolamroth@mail.ru>
2021-04-16 16:36:59 +0300

    Template tag to modify only the selected request.GET parameter

---
+++ django_admin_inline_paginator/templatetags/paginated_inline.py
@@ -0,0 +1,19 @@
+import urllib.parse
+
+from django import template
+
+
+register = template.Library()
+
+
+@register.simple_tag
+def modify_pagination_path(full_path, key, value):
+    get_params = full_path
+    if get_params.find('?') > -1:
+        get_params = get_params[get_params.find('?')+1:]
+    if get_params.find('#') > -1:
+        get_params = get_params[:get_params.find('#')]
+
+    params = urllib.parse.parse_qs(get_params)
+    params[key] = [str(value)]
+    return urllib.parse.urlencode(params, doseq=True)
