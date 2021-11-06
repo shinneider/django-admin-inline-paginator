759f598c9366de1902a21c802ba3fffac0e86274
Shinneider L. Silva <shinneider-libanio@hotmail.com>
2021-11-06 06:00:34 -0300

    bump to 0.2.2

--- django_admin_inline_paginator/__init__.py
+++ django_admin_inline_paginator/__init__.py
@@ -4,7 +4,7 @@ try:
 except ImportError:
     django = None
 
-__version__ = '0.2.1'
+__version__ = '0.2.2'
 
 if django and django.VERSION < (3, 2):  # pragma: no cover
     default_app_config = 'django_admin_inline_paginator.apps.DjangoAdminInlinePaginatorConfig'
